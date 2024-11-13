e2e6e7ebe978b04f98a54ad98b2df7d354f47bed
ShubhzDev <shubhaangiunity@gmail.com>
2024-11-13 17:33:43 +0530

    working on homepage layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,13 @@
 const Header = () => {
     return(
-        <div className="bg-red">
-            <div>img</div>
-            <div>Home</div>
-            <div>Dashborad</div>
-            <div>Contact</div>
-            <div>Sign with discord</div>
+        <div className="grid gap-4 sm:grid-cols-12">
+            <div className="min-h-[100px] bg-orange-500 col-span-4">img</div>
+            <div className="min-h-[100px] bg-red-500 col-span-4 flex flex-row p-4 gap-4">
+                <div>Home</div>
+                <div>Dashborad</div>
+                <div>Contact</div>
+            </div>
+            <div className="min-h-[100px] bg-green-500 col-span-4">Sign with discord</div>
         </div>
     );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}', // Include your JSX/TSX files
-  ],  theme: {
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
     extend: {},
   },
   plugins: [],
 }
-
